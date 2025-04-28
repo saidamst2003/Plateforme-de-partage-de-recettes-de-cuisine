e99b64840e1ccb205f228ba96225af1e77773158
saidamst2003 <saida2003mst@gmail.com>
2025-04-29 00:50:50 +0100

    j'ai  creer nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import{NavComponent} from'./nav/nav.component';
+import{HomeComponent} from'./home/home.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
