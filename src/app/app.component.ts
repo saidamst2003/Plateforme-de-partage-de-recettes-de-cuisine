177fd5cbb84137402acb198eb42befec1c476bd1
saidamst2003 <saida2003mst@gmail.com>
2025-04-29 10:57:06 +0100

    j'ai  creer cage recipe-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import{NavComponent} from'./nav/nav.component';
 import{HomeComponent} from'./home/home.component';
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavComponent,HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, RecipeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
