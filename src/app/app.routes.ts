8143131ecbb7457b5c294be949a426f51de48352
saidamst2003 <saida2003mst@gmail.com>
2025-05-02 13:22:03 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'detail', // make dynamic links (detail/:recetteId)
+        component:RecipeDetailComponent
+    },
+    {
+         path:'list',
+         component :RecipeListComponent
+    
+    },
+    
+];
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+ import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom as angularImportProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+function importProvidersFrom(module: typeof HttpClientModule): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  return angularImportProvidersFrom(module);
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(HttpClientModule)
-  ]
-}).catch(err => console.error(err));
+  providers: [importProvidersFrom(HttpClientModule),provideRouter(routes)]
+});
+    throw new Error('Function not implemented.');
